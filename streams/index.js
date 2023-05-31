b4cc6fd8a952788d03161f70ed3d81116f3d1de9
Oleksii Lysenko <aleksey.lysenko@gmail.com>
2023-05-31 19:23:35 +0300

    - remove obsolete stream implementation

--- streams/index.js
+++ streams/index.js
@@ -1,5 +1,4 @@
 export { ImageWriteStream } from './writable-streams/create-image-writable-stream.js';
-export { run as runAsyncWCallbackWritableStream } from './writable-streams/async-w-callback-writable-stream.js';
 export { runWithImage } from './run-image-writable-stream.js';
 
 export { run as runAllGoodPausedModeReadableStream } from './readable-streams/all-good-paused-mode-readable-streams.js';
